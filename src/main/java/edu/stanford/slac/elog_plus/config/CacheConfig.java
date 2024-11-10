2f2da4bd47aae75ac6dc9cedfe692d81edb6bf69
Claudio Bisegni <bisegni@users.noreply.github.com>
2024-11-10 11:07:29 -0800

    add more log (#301)
    - add more interesting logs

--- src/main/java/edu/stanford/slac/elog_plus/config/CacheConfig.java
+++ src/main/java/edu/stanford/slac/elog_plus/config/CacheConfig.java
@@ -2,15 +2,20 @@ package edu.stanford.slac.elog_plus.config;
 
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Log4j2
 @EnableCaching
 @Configuration
 public class CacheConfig {
     @Bean
     public HazelcastCacheManager cacheManager(HazelcastInstance hazelcastInstance) {
+        // log all configured caches
+        log.info("Configured caches: {}", hazelcastInstance.getConfig().getMapConfigs().keySet());
         return new HazelcastCacheManager(hazelcastInstance);
     }
+
 }
