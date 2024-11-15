f37024d2488e7b375bc6091a06f22c73031ea796
Claudio Bisegni <bisegni@users.noreply.github.com>
2024-11-15 10:37:54 -0800

    logged the event on the cache (#308)
    - add log on cache events

--- src/main/java/edu/stanford/slac/elog_plus/config/CacheConfig.java
+++ src/main/java/edu/stanford/slac/elog_plus/config/CacheConfig.java
@@ -1,6 +1,8 @@
 package edu.stanford.slac.elog_plus.config;
 
+import com.hazelcast.core.EntryEvent;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.listener.EntryEvictedListener;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.cache.annotation.EnableCaching;
@@ -15,7 +17,13 @@ public class CacheConfig {
     public HazelcastCacheManager cacheManager(HazelcastInstance hazelcastInstance) {
         // log all configured caches
         log.info("Configured caches: {}", hazelcastInstance.getConfig().getMapConfigs().keySet());
+
+        hazelcastInstance.getConfig().getMapConfigs().forEach((k, v) -> {
+            log.info("Cache: {} Time to live: {} Max idle: {}", k, v.getTimeToLiveSeconds(), v.getMaxIdleSeconds());
+            v.addEntryListenerConfig(new com.hazelcast.config.EntryListenerConfig()
+                    .setImplementation((EntryEvictedListener<Object, Object>) event -> log.debug("Cache event {} on  {}", event.getEventType(), event.getKey())));
+        });
+
         return new HazelcastCacheManager(hazelcastInstance);
     }
-
 }
